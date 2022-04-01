088c6409bd58814640cac67d6952b29489b4c549
tlatmdcjf94 <tlatmdcjf94@gmail.com>
2022-04-01 17:08:18 +0900

    FBV로 블로그 포스트 상세 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 
+
 def index(request):
     posts = Post.objects.all().order_by('-pk')
 
@@ -9,5 +10,17 @@ def index(request):
         'blog/index.html',
         {
             'posts': posts,
+        }
+    )
+
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'blog/single_post_page.html',
+        {
+            'post': post,
         }
     )
